692641d9222b3612feab95b756465b609138c6c5
Aska Erlangga <ae.askaerlangga@gmail.com>
2021-10-05 11:06:50 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApps());
+}
+
+class MyApps extends StatelessWidget {
+  const MyApps({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Belajar Flutter Dasar'),
+          // backgroundColor: Colors.green,
+          actions: const <Widget>[
+            Icon(Icons.mail),
+          ],
+          ),
+        body: const SafeArea(
+          child: Text('Belajar Dart Dasar')
+        )
+      )
+    );
+  }
+}
