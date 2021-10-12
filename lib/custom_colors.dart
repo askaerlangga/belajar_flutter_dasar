43acd77bfbdf4fa9a18289d4dbd5b801da647161
Aska Erlangga <ae.askaerlangga@gmail.com>
2021-10-12 15:24:34 +0700

    Hello World

--- lib/custom_colors.dart
+++
@@ -1,3 +0,0 @@
-import 'package:flutter/material.dart';
-
-Color softBlack = const Color(0XFF232b2b);
--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,19 @@
 import 'package:flutter/material.dart';
-import 'font_style.dart';
 
 void main() {
-  runApp(const MyApps());
+  runApp(const MyApp());
 }
 
-class MyApps extends StatelessWidget {
-  const MyApps({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({ Key? key }) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Belajar Flutter Dasar'),
-          backgroundColor: Colors.red,
-          ),
-        body: SafeArea(
-          child: Container(
-            // margin: EdgeInsets.all(20),
-            margin: const EdgeInsets.all(0),
-            // padding: EdgeInsets.all(20),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const Image(
-                  image: AssetImage('assets/images/lambang_pdip.jpg'),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 10),
-                  child: Text('Kepak sayap kebhinekaan',
-                    style: mainHeader,
-                    // textAlign: TextAlign.center,
-                  ),
-                ),
-                Text(
-                  'TENG TENG TENG',
-                  style: subHeader
-                  ),
-                const Padding(
-                  padding: EdgeInsets.only(top: 20, right: 10, left: 10),
-                  child: Text(
-                  'Teng teng teng teng teng, \nCoblos gambar banteng. Teng teng teng teng teng, Coblos gambar banteng.',
-                  textAlign: TextAlign.center,
-                  ),
-                ),
-              ],
-            ),
-          ))));
+        appBar: AppBar(title: const Text('Aplikasi Hello World!')),
+        body: Text('Hello World!'),
+      ),
+    );
   }
 }
