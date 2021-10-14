e90832da5d6b003cbd337d0de8fc68b92a18ef09
Aska Erlangga <ae.askaerlangga@gmail.com>
2021-10-14 13:54:23 +0700

    Image Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,29 @@
-import 'dart:math';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({ Key? key }) : super(key: key);
-
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  Random random = Random();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('Latihan Animated Container')),
+        appBar: AppBar(title: Text('Latihan Image')),
         body: Center(
-          child: GestureDetector(
-            onTap: () {
-              setState(() {});
-            },
-            child: AnimatedContainer(
-              duration: Duration(milliseconds: 800),
-              color: Color.fromARGB(255, random.nextInt(256), random.nextInt(256), random.nextInt(256)),
-              width: 50.0 + random.nextInt(101),
-              height: 50.0 + random.nextInt(101),
+          child: Container(
+            color: Colors.black,
+            width: 200,
+            height: 200,
+            padding: EdgeInsets.all(3),
+            child: Image(
+              image: NetworkImage('https://assets.pikiran-rakyat.com/crop/0x0:0x0/x/photo/2020/12/11/3973959611.jpg'),
+              fit: BoxFit.cover,
             ),
           ),
-        ),
+        )
       ),
     );
   }
