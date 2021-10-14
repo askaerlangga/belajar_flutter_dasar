1840969fa559ec236a08b9cd7544734006b7d13a
Aska Erlangga <ae.askaerlangga@gmail.com>
2021-10-14 12:02:03 +0700

    List dan ListView

--- lib/main.dart
+++ lib/main.dart
@@ -4,33 +4,54 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
-class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<Widget> widgets = [];
+  int i = 1;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('Latihan Text Style')),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const <Widget>[
-              Text(
-                'Ini adalah Text',
-                style: TextStyle(
-                  fontFamily: 'Blogger',
-                  fontSize: 20,
-                  color: Colors.red,
-                  decoration: TextDecoration.overline,
-                  decorationColor: Colors.green,
-                  decorationThickness: 5,
-                  decorationStyle: TextDecorationStyle.wavy
-                ),
+        appBar: AppBar(title: Text('Latihan ListView')),
+        body: ListView(children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              ElevatedButton(
+                child: Text('Tambah'),
+                onPressed: () {
+                  setState(() {
+                    widgets.add(
+                        Text('Data ke-$i', style: TextStyle(fontSize: 50)));
+                    i++;
+                  });
+                },
+              ),
+              ElevatedButton(
+                child: Text('Hapus'),
+                onPressed: () {
+                  setState(() {
+                    if (widgets.isNotEmpty) {
+                      widgets.removeLast();
+                      i--;
+                    }
+                  });
+                },
               ),
             ],
           ),
-        ),
+          Column(
+            children: widgets,
+          )
+        ]),
       ),
     );
   }
