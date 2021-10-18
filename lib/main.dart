849c7e1f6b4568267f67ae4da3e90bbed75c49f1
Aska Erlangga <ae.askaerlangga@gmail.com>
2021-10-18 22:30:38 +0700

    TextField

--- lib/main.dart
+++ lib/main.dart
@@ -3,37 +3,41 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
-class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  TextEditingController controlText = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.red,
-        body: Container(
-          margin: EdgeInsets.all(10),
-          child: ListView(
-            children: <Widget>[
-              buildCard(Icons.adb, 'Android'),
-              buildCard(Icons.account_box, 'Account Box'),
-            ],
-          ),
-        )
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('Latihan Text Field'),
       ),
-    );
-  }
-
-  Card buildCard(IconData iconData, String text) {
-    return Card(
-              elevation: 5,
-              child: Row(
-                children: <Widget>[
-                  Container(margin: EdgeInsets.all(5)),
-                  Icon(iconData, color: Colors.red),
-                  Text(text),
-                ],
-              ),
-            );
+      body: Container(
+        margin: EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            TextField(
+              // obscureText: true,
+              maxLength: 10,
+              onChanged: (value) {
+                setState(() {});
+              },
+              controller: controlText,
+            ),
+            Text(controlText.text),
+          ],
+        ),
+      ),
+    ));
   }
 }
