0c5d616bd69e1608e43bb10d9a46df2d60a97521
Aska Erlangga <ae.askaerlangga@gmail.com>
2021-10-19 21:35:13 +0700

    MediaQuery Responsive App

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,16 @@
+import 'package:belajar_flutter_dasar/main_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
-
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  TextEditingController controlText = TextEditingController();
+class MyApp extends StatelessWidget {
+  const MyApp({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: Text('Latihan Text Field'),
-      ),
-      body: Container(
-        margin: EdgeInsets.all(20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-            TextField(
-              decoration: InputDecoration(
-                // icon: Icon(Icons.person),
-                prefixIcon: Icon(Icons.person),
-                hintText: 'Username',
-                border: OutlineInputBorder(borderRadius: BorderRadius.circular(10.0)),
-              ),
-              // obscureText: true,
-              // maxLength: 10,
-              onChanged: (value) {
-                setState(() {});
-              },
-              controller: controlText,
-            ),
-            Text(controlText.text),
-          ],
-        ),
-      ),
-    ));
+      home: MainPage(),
+    );
   }
 }
---
+++ lib/main_page.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text('Latihan Media Query')),
+        // body: Container(
+        //   color: Colors.red,
+        //   width: MediaQuery.of(context).size.width / 2,
+        //   height: MediaQuery.of(context).size.height / 2,
+        // ),
+        body: (MediaQuery.of(context).orientation == Orientation.portrait)
+            ? Column(
+                children: generateContainer,
+              )
+            : Row(
+              children: generateContainer,
+            ));
+  }
+
+  List<Widget> get generateContainer {
+    return <Widget>[
+      Container(color: Colors.red, width: 100, height: 100),
+      Container(color: Colors.blue, width: 100, height: 100),
+      Container(color: Colors.green, width: 100, height: 100),
+    ];
+  }
+}
