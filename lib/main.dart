0de7a770b9337e9224ada7419a4fe4c79b64e647
Aska Erlangga <ae.askaerlangga@gmail.com>
2021-10-19 22:06:48 +0700

    Elevate dan Material Button

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,41 @@
-import 'package:belajar_flutter_dasar/main_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MainPage(),
+      home: Scaffold(
+        appBar: AppBar(title: Text('Latihan Membuat Button')),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              ElevatedButton(
+                onPressed: () {},
+                child: Text('Elevate Button'),
+              ),
+              MaterialButton(
+                onPressed: () {},
+                child: Text(
+                  'Material Button',
+                  style: TextStyle(color: Colors.white),
+                ),
+                color: Colors.red,
+                splashColor: Colors.redAccent,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)),
+              ),
+              Container(),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
