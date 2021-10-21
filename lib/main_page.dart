5e2f44be81f8be7f8a4943de817c358371ab73dc
Aska Erlangga <ae.askaerlangga@gmail.com>
2021-10-21 22:43:56 +0700

    Cover Image

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -9,7 +9,7 @@ class MainPage extends StatelessWidget {
       appBar: AppBar(
         title:
             Text('Custom Card Example', style: TextStyle(color: Colors.white)),
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.red.shade900,
       ),
       body: Stack(
         children: <Widget>[
@@ -19,6 +19,32 @@ class MainPage extends StatelessWidget {
                     colors: [Colors.redAccent, Colors.redAccent.shade100],
                     begin: Alignment.topCenter,
                     end: Alignment.bottomCenter)),
+          ),
+          Center(
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width * 0.9,
+              height: MediaQuery.of(context).size.height * 0.7,
+              child: Card(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8)),
+                elevation: 10,
+                child: Stack(
+                  children: <Widget>[
+                    Container(
+                      height: MediaQuery.of(context).size.height * 0.35,
+                      decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(8),
+                              topRight: Radius.circular(8)),
+                          image: DecorationImage(
+                              image: NetworkImage(
+                                  'https://images.unsplash.com/photo-1633882228840-e847b7694baf?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1374&q=80'),
+                              fit: BoxFit.cover)),
+                    ),
+                  ],
+                ),
+              ),
+            ),
           )
         ],
       ),
