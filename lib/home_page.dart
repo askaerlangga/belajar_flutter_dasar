b7e084f3db2ebf77dcf314e77ab8c931d0cc9cfb
Aska Erlangga <ae.askaerlangga@gmail.com>
2021-10-25 21:54:09 +0700

    TextField Email dan Password

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({ Key? key }) : super(key: key);
-  double smallDiameter(BuildContext context) => MediaQuery.of(context).size.width * 2 / 3;
-  double bigDiameter(BuildContext context) => MediaQuery.of(context).size.width * 7 / 8;
-
+  const HomePage({Key? key}) : super(key: key);
+  double smallDiameter(BuildContext context) =>
+      MediaQuery.of(context).size.width * 2 / 3;
+  double bigDiameter(BuildContext context) =>
+      MediaQuery.of(context).size.width * 7 / 8;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey.shade200,
       body: Stack(
         children: <Widget>[
           Positioned(
@@ -18,13 +20,12 @@ class HomePage extends StatelessWidget {
               width: smallDiameter(context),
               height: smallDiameter(context),
               decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                gradient: LinearGradient(
-                  colors: [Colors.yellow, Colors.blue],
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                )
-              ),
+                  shape: BoxShape.circle,
+                  gradient: LinearGradient(
+                    colors: [Colors.yellow, Colors.blue],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                  )),
             ),
           ),
           Positioned(
@@ -32,24 +33,65 @@ class HomePage extends StatelessWidget {
             top: -bigDiameter(context) / 4,
             child: Container(
               child: Center(
-                child: Text(
-                  'My Apps',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 30,
-                  )
-                )
-              ),
+                  child: Text('My Apps',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30,
+                      ))),
               width: bigDiameter(context),
               height: bigDiameter(context),
               decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                gradient: LinearGradient(
-                  colors: [Colors.yellow.shade600, Colors.blue],
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                )
-              ),
+                  shape: BoxShape.circle,
+                  gradient: LinearGradient(
+                    colors: [Colors.yellow.shade600, Colors.blue],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                  )),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: ListView(
+              children: <Widget>[
+                Container(
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        border: Border.all(color: Colors.grey),
+                        borderRadius: BorderRadius.circular(5)),
+                    margin: EdgeInsets.fromLTRB(20, 300, 20, 10),
+                    padding: EdgeInsets.fromLTRB(10, 10, 10, 25),
+                    child: Column(children: <Widget>[
+                      TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Email',
+                            prefixIcon: Icon(Icons.email, color: Colors.blue),
+                            focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.grey))),
+                      ),
+                      TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          hintText: 'Password',
+                            prefixIcon: Icon(Icons.lock, color: Colors.blue),
+                            focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.grey))),
+                      ),
+                      
+                    ])),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: Container(
+                        margin: EdgeInsets.only(right: 20, bottom: 20),
+                        child: Text(
+                          'Forgot Password',
+                          style: TextStyle(
+                            color: Colors.blue,
+                          ),
+                          
+                        ),
+                      ),
+                    ),
+              ],
             ),
           )
         ],
