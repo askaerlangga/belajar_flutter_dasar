71c4fd1b605eaf7a660da50b0fdf0a4bf13814d8
Aska Erlangga <ae.askaerlangga@gmail.com>
2022-05-22 14:33:32 +0700

    TabBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,48 @@
-import 'package:belajar_flutter_dasar/page/home_page.dart';
-import 'package:belajar_flutter_dasar/page/post_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: 'HTTP Request',
-    initialRoute: '/home',
-    routes: {
-      '/home': (context) => const HomePage(),
-      '/post': (context) => const PostPage(),
-    },
-  ));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text('Latihan TabBar'),
+            bottom: const TabBar(tabs: [
+              Tab(
+                icon: Icon(Icons.home),
+                text: 'Home',
+              ),
+              Tab(
+                icon: Icon(Icons.chat),
+                text: 'Chat',
+              ),
+              Tab(
+                icon: Icon(Icons.call),
+                text: 'Call',
+              )
+            ]),
+          ),
+          body: const TabBarView(children: [
+            Center(
+              child: Text('Tab 1'),
+            ),
+            Center(
+              child: Text('Tab 2'),
+            ),
+            Center(
+              child: Text('Tab 3'),
+            )
+          ]),
+        ),
+      ),
+    );
+  }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:belajar_flutter_dasar/main.dart';
+// import 'package:belajar_flutter_dasar/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
