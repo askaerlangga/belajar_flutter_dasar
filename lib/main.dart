316c2930a5a918c88ed40ddd93308680067261cf
Aska Erlangga <ae.askaerlangga@gmail.com>
2022-05-23 21:11:09 +0700

    Switch

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,3 @@
-import 'dart:math';
-
-import 'package:belajar_flutter_dasar/model/person.dart';
-import 'package:belajar_flutter_dasar/model/repository.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,77 +12,41 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  Person person = Person();
-  Repository repository = Repository();
+  bool isON = false;
+  String text = 'Saklar OFF';
+  Widget myWidget = const Text(
+    'Saklar OFF',
+    style: TextStyle(color: Colors.red, fontSize: 20),
+  );
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('HTTP Request'),
+          title: const Text('Switch'),
         ),
         body: Center(
           child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'ID : ',
-                  style: TextStyle(fontSize: 20),
-                ),
-                Text(
-                  person.id ?? 'Belum ada data : ',
-                  style: TextStyle(fontSize: 20),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Name : ',
-                  style: TextStyle(fontSize: 20),
-                ),
-                Text(
-                  person.name ?? 'Belum ada data : ',
-                  style: TextStyle(fontSize: 20),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Job : ',
-                  style: TextStyle(fontSize: 20),
-                ),
-                Text(
-                  person.job ?? 'Belum ada data : ',
-                  style: TextStyle(fontSize: 20),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'CreatedAt : ',
-                  style: TextStyle(fontSize: 20),
-                ),
-                Text(
-                  person.createdAt ?? 'Belum ada data : ',
-                  style: TextStyle(fontSize: 20),
-                ),
-              ],
-            ),
-            ElevatedButton(
-                onPressed: () async {
-                  await repository.postData('Aska', 'Mahasiswa').then((value) {
-                    person = value;
-                    setState(() {});
+            myWidget,
+            Switch(
+                activeColor: Colors.green,
+                inactiveThumbColor: Colors.red,
+                value: isON,
+                onChanged: (value) {
+                  isON = value;
+                  setState(() {
+                    isON
+                        ? myWidget = const Text(
+                            'Saklar ON',
+                            style: TextStyle(color: Colors.green, fontSize: 20),
+                          )
+                        : myWidget = const Text(
+                            'Saklar OFF',
+                            style: TextStyle(color: Colors.red, fontSize: 20),
+                          );
                   });
-                },
-                child: Text('POST Data'))
+                })
           ]),
         ),
       ),
