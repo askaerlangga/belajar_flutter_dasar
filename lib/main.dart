20cd716a9378c496537640c8d5cee9382dda97f6
Aska Erlangga <ae.askaerlangga@gmail.com>
2022-05-23 12:49:32 +0700

    AudioPlayer

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,103 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
-import 'package:qr_flutter/qr_flutter.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String durasi = '00:00:00';
+  AudioPlayer audioPlayer = AudioPlayer();
+  _MyAppState() {
+    audioPlayer = AudioPlayer();
+    audioPlayer.onAudioPositionChanged.listen((duration) {
+      setState(() {
+        durasi = duration.toString();
+      });
+      audioPlayer.setReleaseMode(ReleaseMode.LOOP);
+    });
+  }
+
+  void playSound(String url) async {
+    await audioPlayer.play(url);
+  }
+
+  void pauseSound() async {
+    await audioPlayer.pause();
+  }
+
+  void stopSound() async {
+    await audioPlayer.stop();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(title: Text('Gradient Opacity')),
-      body: Center(
-          child: ShaderMask(
-        shaderCallback: (rectangle) {
-          return LinearGradient(
-                  colors: [Colors.black, Colors.transparent],
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter)
-              .createShader(
-                  Rect.fromLTRB(0, 0, rectangle.width, rectangle.height));
-        },
-        blendMode: BlendMode.dstIn,
-        child: Image(
-          width: 300,
-          image: NetworkImage(
-              'https://cdn.pixabay.com/photo/2018/04/23/09/08/panorama-3343622_960_720.jpg'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Play Sound'),
         ),
-      )),
-    ));
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(50)),
+                    child: IconButton(
+                      onPressed: () {
+                        playSound(
+                            'https://upload.wikimedia.org/wikipedia/commons/6/6f/Indonesia_Raya_Simfoni_dan_Vokal_1_Bait.ogg');
+                      },
+                      icon: Icon(Icons.play_arrow),
+                      color: Colors.white,
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(50)),
+                    child: IconButton(
+                      onPressed: () {
+                        pauseSound();
+                      },
+                      icon: Icon(Icons.pause),
+                      color: Colors.white,
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(50)),
+                    child: IconButton(
+                      onPressed: () {
+                        stopSound();
+                      },
+                      icon: Icon(Icons.stop),
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+              Text(
+                durasi,
+                style: TextStyle(fontSize: 20),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
