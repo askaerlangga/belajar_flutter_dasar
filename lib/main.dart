a1a438230c70a46fe9227d611273ae474d0a7119
Aska Erlangga <ae.askaerlangga@gmail.com>
2022-05-24 12:27:29 +0700

    Provider State Management

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,57 @@
+import 'package:belajar_flutter_dasar/application_color.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  bool isON = false;
-  String text = 'Saklar OFF';
-  Widget myWidget = const Text(
-    'Saklar OFF',
-    style: TextStyle(color: Colors.red, fontSize: 20),
-  );
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Switch'),
-        ),
-        body: Center(
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            myWidget,
-            Switch(
-                activeColor: Colors.green,
-                inactiveThumbColor: Colors.red,
-                value: isON,
-                onChanged: (value) {
-                  isON = value;
-                  setState(() {
-                    isON
-                        ? myWidget = const Text(
-                            'Saklar ON',
-                            style: TextStyle(color: Colors.green, fontSize: 20),
-                          )
-                        : myWidget = const Text(
-                            'Saklar OFF',
-                            style: TextStyle(color: Colors.red, fontSize: 20),
-                          );
-                  });
-                })
-          ]),
+      home: ChangeNotifierProvider<ApplicationColor>(
+        create: (context) => ApplicationColor(),
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.black,
+            title: Consumer<ApplicationColor>(
+                builder: (context, applicationColor, _) => Text(
+                      'Provider State Management',
+                      style: TextStyle(color: applicationColor.color),
+                    )),
+          ),
+          body: Center(
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Consumer<ApplicationColor>(
+                builder: (context, applicationColor, _) => AnimatedContainer(
+                  duration: Duration(milliseconds: 500),
+                  margin: EdgeInsets.all(5),
+                  width: 100,
+                  height: 100,
+                  color: applicationColor.color,
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('AB'),
+                  Consumer<ApplicationColor>(
+                      builder: (context, applicationColor, _) => Switch(
+                          activeColor: applicationColor.color,
+                          inactiveThumbColor: applicationColor.color,
+                          value: applicationColor.isLightBlue,
+                          onChanged: (value) {
+                            applicationColor.isLightBlue = value;
+                          })),
+                  Text('LB')
+                ],
+              )
+            ]),
+          ),
         ),
       ),
     );
