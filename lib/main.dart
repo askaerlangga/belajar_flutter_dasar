db327b6a6d3aaad23a57dacd0de2398597a79d08
Aska Erlangga <ae.askaerlangga@gmail.com>
2022-05-27 15:02:03 +0700

    Doc Comment

--- lib/main.dart
+++ lib/main.dart
@@ -1,62 +1,21 @@
-import 'package:belajar_flutter_dasar/color_bloc.dart';
+import 'package:belajar_flutter_dasar/person.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+Person person1 = Person('Aska', 'Serang');
+
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  void dispose() {
-    bloc.dispose();
-    super.dispose();
-  }
-
-  ColorBloc bloc = ColorBloc();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        floatingActionButton:
-            Row(mainAxisAlignment: MainAxisAlignment.end, children: [
-          FloatingActionButton(
-              backgroundColor: Colors.amber,
-              onPressed: () {
-                bloc.eventSink.add(ColorEvent.to_amber);
-              }),
-          SizedBox(
-            width: 10,
-          ),
-          FloatingActionButton(
-              backgroundColor: Colors.blue,
-              onPressed: () {
-                bloc.eventSink.add(ColorEvent.to_blue);
-              })
-        ]),
-        appBar: AppBar(
-          title: Text('Bloc State Management'),
-        ),
-        body: Center(
-            child: StreamBuilder(
-          stream: bloc.stateStream,
-          initialData: Colors.amber,
-          builder: (context, AsyncSnapshot<Color> snapshot) {
-            return AnimatedContainer(
-              width: 100,
-              height: 100,
-              color: snapshot.data,
-              duration: Duration(milliseconds: 500),
-            );
-          },
-        )),
-      ),
+          appBar: AppBar(
+        title: Text(person1.name),
+      )),
     );
   }
 }
---
+++ lib/person.dart
@@ -0,0 +1,12 @@
+class Person {
+  /// Digunakan untuk menyimpan nama
+  final String name;
+
+  /// Digunakan untuk menyimpan alamat
+  final String address;
+
+  /// Digunakan untuk membuat object Person
+  /// * [name] berisi nama person
+  /// * [address] berisi alamat person
+  Person(this.name, this.address);
+}
