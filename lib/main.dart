8fb60a5fabb45b1cb18b1e94a02daa765c6dfe5b
Aska Erlangga <ae.askaerlangga@gmail.com>
2022-05-29 20:23:28 +0700

    SelectableText

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,36 @@
-import 'dart:async';
-
-import 'package:belajar_flutter_dasar/progress_bar.dart';
-import 'package:belajar_flutter_dasar/time_state.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Custom Progress Bar'),
+          title: Text('Judul Aplikasi'),
         ),
         body: Center(
-          child: ChangeNotifierProvider(
-            create: (context) => TimeState(),
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Consumer<TimeState>(
-                  builder: (context, timeState, _) => CustomProgressBar(
-                    200,
-                    timeState.time,
-                    15,
-                  ),
-                ),
-                Consumer<TimeState>(
-                    builder: (context, timeState, _) => ElevatedButton(
-                        onPressed: () {
-                          Timer.periodic(Duration(seconds: 1), (timer) {
-                            (timeState.time == 0)
-                                ? timer.cancel()
-                                : timeState.time -= 1;
-                          });
-                        },
-                        child: Text('Start')))
-              ],
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Ini adalah text Biasa'),
+            SizedBox(
+              height: 10,
             ),
-          ),
-        ),
+            SelectableText(
+                'Ini adalah Selectable Text, silahkan tekan dan tahan'),
+          ],
+        )),
       ),
     );
   }
