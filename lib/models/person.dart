b7eaabcf7a1ebdc37930b88cadd1cd35423b42ce
Aska Erlangga <ae.askaerlangga@gmail.com>
2022-06-03 22:27:35 +0700

    GetX Unique ID

--- lib/models/person.dart
+++
@@ -1,5 +0,0 @@
-class Person {
-  var name;
-  var address;
-  Person({this.name = 'No Name', this.address = 'No Address'});
-}
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,30 +1,45 @@
-import 'package:belajar_flutter_dasar/controllers/person_controller.dart';
+import 'package:belajar_flutter_dasar/controllers/count_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({Key? key}) : super(key: key);
+  final counterController = Get.put(CounterController());
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          counterController.increment();
+        },
+      ),
       appBar: AppBar(
         title: const Text('GetX'),
       ),
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            GetBuilder<PersonController>(
-                init: PersonController(),
-                builder: ((controller) => Text(
-                      controller.person.name,
-                      style: const TextStyle(fontSize: 20),
-                    ))),
-            ElevatedButton(
-                onPressed: () {
-                  Get.find<PersonController>().changeUpperCase();
-                },
-                child: Text('Change'))
+            const Text(
+              'ANGKA',
+              style: TextStyle(fontSize: 20),
+            ),
+            GetBuilder<CounterController>(
+              // Unique ID
+              id: '01',
+              builder: (_) => Text(
+                counterController.counter.toString(),
+                style: const TextStyle(fontSize: 100),
+              ),
+            ),
+            GetBuilder<CounterController>(
+              builder: (_) => Text(
+                counterController.counter.toString(),
+                style: const TextStyle(fontSize: 100),
+              ),
+            )
           ],
         ),
       ),
