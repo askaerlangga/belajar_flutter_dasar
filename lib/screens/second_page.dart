da42738c5788d4ae0fc1e3d6139dbbf01ff72362
Aska Erlangga <ae.askaerlangga@gmail.com>
2022-06-06 15:24:24 +0700

    GetX Workers

--- lib/screens/second_page.dart
+++ lib/screens/second_page.dart
@@ -7,10 +7,38 @@ class SecondPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Second Page')),
+      appBar: AppBar(
+        title: Text('Second Page'),
+        actions: <Widget>[
+          IconButton(
+              onPressed: () {
+                textFieldController.reset();
+                // textFieldController.textController.clear();
+              },
+              icon: Icon(Icons.refresh))
+        ],
+      ),
       body: Center(
-          child: TextField(
-        controller: textFieldController.textController,
+          child: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Obx(() =>
+                Text('Terjadi sesuatu : ${textFieldController.count.value}')),
+            Container(
+              margin: EdgeInsets.all(10),
+              child: Obx(
+                () => TextField(
+                  controller: textFieldController.textController.value,
+                  onChanged: (_) => textFieldController.change(),
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(borderSide: BorderSide())),
+                ),
+              ),
+            ),
+          ],
+        ),
       )),
     );
   }
